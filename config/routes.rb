3b34862efb6217625f724c0e2000600b92ad8e66
A-Glazer <avigayilglazer@gmail.com>
2020-02-18 12:15:14 +0200

    Updated routes to make slots and comments nested under babysitters

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   
   namespace :api do 
     namespace :v1 do
-      resources :babysitters
-      resources :slots
-      resources :comments
+      resources :babysitters do 
+        resources :slots
+        resources :comments
+      end
+        
     end
   end
 end
