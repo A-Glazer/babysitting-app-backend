fb93ddc51fc82e29df88219c7cb5094a41abb795
A-Glazer <avigayilglazer@gmail.com>
2020-02-18 22:25:17 +0200

    Uncommented before_action :find_babysitter

--- app/controllers/api/v1/babysitters_controller.rb
+++ app/controllers/api/v1/babysitters_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BabysittersController < ApplicationController
 
-    # before_action :find_babysitter, only: [:show, :destroy]
+    before_action :find_babysitter, only: [:show, :destroy]
 
     def index
         # binding.pry
