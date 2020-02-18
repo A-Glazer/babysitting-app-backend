2bb49bb4a4547a3ee77b0c11b275ff68c7da201c
A-Glazer <avigayilglazer@gmail.com>
2020-02-18 12:01:24 +0200

    Added crud methods to comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,33 @@
 class API::V1::CommentsController < ApplicationController
+
+    def index
+        @comments = Comment.all
+        render json: @comments
+    end
+
+    def create
+        @comment = Comment.new(comment_params)
+        if @comment.save
+            render json: @comment
+        else
+            render json: {error: "Error creating a new comment"}
+        end
+    end
+
+    def show
+        @comment = Comment.find(params[:id])
+        render json: @comment
+    end
+
+    def destroy 
+        @comment = Comment.find(params[:id])
+        @comment.destroy 
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:babysitter_id, :message, :name)
+    end
+
 end
