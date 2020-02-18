0d03db2f40e4324a6eb7f650a6cdceb6f4bac46c
A-Glazer <avigayilglazer@gmail.com>
2020-02-18 12:02:58 +0200

    Added crud methods to slots controller

--- app/controllers/api/v1/slots_controller.rb
+++ app/controllers/api/v1/slots_controller.rb
@@ -1,2 +1,31 @@
 class API::V1::SlotsController < ApplicationController
+    def index
+        @slots = Slot.all
+        render json: @slots
+    end
+
+    def create
+        @slot = Slot.new(slot_params)
+        if @slot.save
+            render json: @slot
+        else
+            render json: {error: "Error creating a new slot"}
+        end
+    end
+
+    def show
+        @slot = Slot.find(params[:id])
+        render json: @slot
+    end
+
+    def destroy 
+        @slot = Slot.find(params[:id])
+        @slot.destroy 
+    end
+
+    private
+
+    def slot_params
+        params.require(:slot).permit(:babysitter_id, :day_of_week, :time_of_day)
+    end
 end
