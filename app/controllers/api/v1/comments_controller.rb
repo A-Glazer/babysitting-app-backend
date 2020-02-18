f6b521f88a2e686aa59331ef9d211edf4f640c26
A-Glazer <avigayilglazer@gmail.com>
2020-02-18 12:12:48 +0200

    added before_action and index method to comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,30 +1,29 @@
 class API::V1::CommentsController < ApplicationController
+    
+    before_action :set_babysitter
 
     def index
-        @comments = Comment.all
-        render json: @comments
+      @comments = @babysitter.comments
+      render json: @comments
     end
 
     def create
-        @comment = Comment.new(comment_params)
-        if @comment.save
-            render json: @comment
-        else
-            render json: {error: "Error creating a new comment"}
-        end
+       
     end
 
     def show
-        @comment = Comment.find(params[:id])
-        render json: @comment
+       
     end
 
     def destroy 
-        @comment = Comment.find(params[:id])
-        @comment.destroy 
+       
     end
 
     private
+
+    def set_babysitter
+        @babysitter = Babysitter.find(params[:babysitter_id])
+    end
 
     def comment_params
         params.require(:comment).permit(:babysitter_id, :message, :name)
