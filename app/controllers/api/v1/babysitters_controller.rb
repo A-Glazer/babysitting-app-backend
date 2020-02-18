94ece61343e09c14b3b5af952d65638c76cd18d2
A-Glazer <avigayilglazer@gmail.com>
2020-02-18 11:52:20 +0200

    Added crud to babysitter controller

--- app/controllers/api/v1/babysitters_controller.rb
+++ app/controllers/api/v1/babysitters_controller.rb
@@ -1,2 +1,25 @@
 class API::V1::BabysittersController < ApplicationController
+
+    def index
+        @babysitters = Babysitter.all
+        render json: @babysitters
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def destroy 
+
+    end
+
+    private
+
+    def babysitter_params
+        params.require(:babysitter).permit(:first_name, :last_name, :phone_number)
+    end
 end
